cb52540da677909888137cf796a651bd37700247
John Gannon <johnpgannon@gmail.com>
2021-02-16 13:29:10 -0500

    asdf

--- RealTime_Water_Storage/Test Script.R
+++ RealTime_Water_Storage/Test Script.R
@@ -1,1 +1,6 @@
 #Hello this is a test
+
+library(tidyverse)
+
+data <- read_csv("RealTime_Water_Storage/RealTime_Water_Storage/Water_table_WS3upper_WS_3Up_wells.dat",
+                 skip = 4, col_names = c())
