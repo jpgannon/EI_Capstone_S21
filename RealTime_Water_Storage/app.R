b647f1bf5d0e0411dd2d0bfb86c90c59122cabb0
John Gannon <johnpgannon@gmail.com>
2021-02-23 13:49:47 -0500

    first plot

--- RealTime_Water_Storage/app.R
+++ RealTime_Water_Storage/app.R
@@ -11,10 +11,25 @@
 #load libraries
 library(shiny)
 library(leaflet)
-library(waterData)
-library(plotly)
+#library(waterData)
+#library(plotly)
 library(DT) #MU: Helpful for displaying data tables.
 library(tidyverse) #MU: I added tidyverse because it has ggplot2 and other good functions 
+
+#reading in WS3 well data
+setwd("/Volumes/GoogleDrive/My Drive/CLASSES/EI Capstone/EI_Capstone_S21")
+ws3_upper_wells <- read_csv("RealTime_Water_Storage/RealTime_Water_Storage/Water_table_WS3upper_WS_3Up_wells.dat",
+                            skip = 4, col_names = c(X1 = "TIMESTAMP" , X2 = "RECORD", X3 ="Batt_Volt", 
+                                                    X4= "ptemp_Max" , X5= "WS3_N1_psi",X6="WS3_N1_rawdepth",
+                                                    X7="WS3_N1_depth_corr",X8="WS3_N1_corr_depth",
+                                                    X9="WS3_N1_welltemp",X10="WS3_N2_psi",
+                                                    X11="WS3_N2_rawdepth",X12="WS3_N2_depth_corr",
+                                                X13="WS3_N2_corr_depth",X14="WS3_N2_welltemp",
+                                                X15="WS3_42_4_d2_psi",X16="WS3_42_4_d2_rawdepth",
+                                                X17="WS3_42_4_d2_depth_corr",X18="WS3_42_4_d2_corr_depth",
+                                                X19="WS3_42_4_d2_welltemp"))%>%
+    select(TIMESTAMP, WS3_N1_corr_depth, WS3_N2_corr_depth, WS3_42_4_d2_corr_depth) %>%
+    pivot_longer(cols = c(WS3_N1_corr_depth, WS3_N2_corr_depth, WS3_42_4_d2_corr_depth))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -27,6 +42,9 @@ ui <- fluidPage(
         sidebarPanel(
             dateInput("startdate", label = "Start Date", val=""), #MU: Should we make the default start value the first data present in the data we read in?
             dateInput("enddate", label= "End Date", value=Sys.Date(), max=Sys.Date()),
+            selectInput(inputId = "toview", label = "Select dataset to view:", 
+                        choices = unique(ws3_upper_wells$name), 
+                        selected = unique(ws3_upper_wells$name)[1]),
             width = 3
         ),
 
@@ -36,7 +54,7 @@ ui <- fluidPage(
                tabPanel('About',h4("This app visualizes real-time data from Hubbard Brook research watershed sites 3 and 9, 
                                    as well as related snowpack and weather conditions, for a date range selected to compare the two sites
                                  or by certain perameters."),
-               #tabPanel('Watershed Visualizations', plotlyOutput),
+               tabPanel('Watershed Visualizations', plotOutput("plot1")),
                tabPanel('Table' ,tableOutput("table"))#,
                #tabPanel('Map of Stations', leafletOutput("map",width = '100%'))
            )
@@ -53,7 +71,11 @@ server <- function(input, output) {
                              class = "display nowrap compact", #MU: this is the style of the table
                              filter = "top") #MU: This places the filter at the top of the table
     #MU: This is a placeholder table for when we finish cleaning the data and can input summarized values
-    
+    output$plot1 <- renderPlot({
+        ws3_upper_wells %>% filter(name == input$toview) %>%
+            ggplot(aes(x = TIMESTAMP, y = value))+
+            geom_line()
+    })
 }
 
 # Run the application 
