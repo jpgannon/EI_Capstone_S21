da01ba53b7909305dccc2d5f41fa3776730e9a4d
Michelle Uchitel <muchitel@vt.edu>
2021-03-02 17:45:56 -0500

    Converted Well data from 5 minute to hourly. -MU

--- RealTime_Water_Storage/app.R
+++ RealTime_Water_Storage/app.R
@@ -28,9 +28,12 @@ ws3_upper_wells <- read_csv("Realtime_waterstorage_app/Water_Storage_Data/Water_
                                                 X15="WS3_42_4_d2_psi",X16="WS3_42_4_d2_rawdepth",
                                                 X17="WS3_42_4_d2_depth_corr",X18="WS3_42_4_d2_corr_depth",
                                                 X19="WS3_42_4_d2_welltemp"))%>%
-    select(TIMESTAMP, WS3_N1_corr_depth, WS3_N2_corr_depth, WS3_42_4_d2_corr_depth) %>%
-  pivot_longer(cols = c(WS3_N1_corr_depth, WS3_N2_corr_depth, WS3_42_4_d2_corr_depth))
+    select(TIMESTAMP, WS3_N1_corr_depth, WS3_N2_corr_depth, WS3_42_4_d2_corr_depth)  
 
+ws3_upper_wells <-  ws3_upper_wells %>% 
+                    aggregate(list(TIME = cut(ws3_upper_wells$TIMESTAMP, breaks="hour")), 
+                            mean, na.rm = TRUE) %>%  #MU: I think I aggregated this correctly so it shows hourly data but not positive. 
+                    select(TIME, WS3_N1_corr_depth, WS3_N2_corr_depth, WS3_42_4_d2_corr_depth)
 
 #reading in WS9 well data
 
@@ -46,6 +49,11 @@ ws9_upper_wells <- read_csv("Realtime_waterstorage_app/Water_Storage_Data/Water_
                                                     X17= "HB176d_depth_corr" , X18= "HB176d_corr_depth", 
                                                     X19 = "HB176d_welltemp"))%>%
   select(TIMESTAMP, HB156_corr_depth, HB179s_corr_depth, HB176d_corr_depth) 
+
+ws9_upper_wells <-  ws9_upper_wells %>% 
+  aggregate(list(TIME = cut(ws9_upper_wells$TIMESTAMP, breaks="hour")), 
+            mean, na.rm = TRUE) %>%  #MU: I think I aggregated this correctly so it shows hourly data but not positive. 
+  select(TIME, HB156_corr_depth, HB179s_corr_depth, HB176d_corr_depth)
 
 
 
@@ -154,36 +162,8 @@ tabPanel('Watershed Visualizations',
       ),
 
   
-tabPanel('Porosity Slider', 
-         sidebarLayout(
-           sidebarPanel(width = 4,
-                        sliderInput(inputId = "poros",label = "Porosity:",
-                                    min = 0,max = 1,value = 0.5, step = 0.01),
-                        sliderInput(inputId = "change",label = "% change",
-                                    min = -100,max = 100,value = 0)
-           ),
-           
-           mainPanel(
-             plotOutput('porosPlot')
-           )
-         )
-      ),
-
-       
-      
-
-  
 tabPanel('Table View' ,DTOutput("table")),
 
-#just experimental, not close to being real calculations - SL
-#scratch this
-#tabPanel('Porosity Slider', 
-
-        
-       
-
-          
-        
          
 tabPanel('Map', leafletOutput("map",width = '100%'))
 
@@ -204,7 +184,7 @@ server <- function(input, output) {
   #  mutate(standardized_well_1 = ((WS3_N1_corr_depth * 10) * input$poros)) %>% 
   #  mutate(standardized_well_2 = ((WS3_N2_corr_depth * 10) * input$poros)) %>% 
   #  mutate(standardized_deep_well = ((WS3_42_4_d2_corr_depth * 10) * input$poros)) %>%
-   # select(TIMESTAMP, standardized_well_1, standardized_well_2, standardized_deep_well)
+   # select(TIME, standardized_well_1, standardized_well_2, standardized_deep_well)
   
   
   #Mu: standardized well ws9 data to mm H2O
@@ -213,19 +193,22 @@ server <- function(input, output) {
     mutate(standardized_well_1 = ((HB156_corr_depth * 10) * input$poros)) %>% 
     mutate(standardized_well_2 = ((HB179s_corr_depth * 10) * input$poros)) %>% 
     mutate(standardized_deep_well = ((HB176d_corr_depth * 10) * input$poros)) %>%
-    select(TIMESTAMP, standardized_well_1, standardized_well_2, standardized_deep_well)
+    select(TIME, standardized_well_1, standardized_well_2, standardized_deep_well)
   })
   #MU: standardized snow ws3 data to mm H2O
   standardized_SnowHr_WS3 <-  reactive({
     ws3_upper_snowdat_hr %>% 
-    mutate(standardized_snow = (((H2O_Content_1_Avg + H2O_Content_2_Avg) / 2) * (Depthscaled_Avg * 10)) * input$density)
+    mutate(standardized_snow = (((H2O_Content_1_Avg + H2O_Content_2_Avg) / 2) * (Depthscaled_Avg * 10)))
   })
   
   #MU: standardized snow ws9 data to mm H2O
   standardized_SnowHr_WS9 <- reactive({
     ws9_upper_snowdat_hr %>% 
-    mutate(standardized_snow = (((H2O_Content_1_Avg + H2O_Content_2_Avg) / 2) * (Depthscaled_Avg * 10)) * input$density)
+    mutate(standardized_snow = (((H2O_Content_1_Avg + H2O_Content_2_Avg) / 2) * (Depthscaled_Avg * 10)))
  })
+  
+  #MU: standardized precip data ws3 to mm H2O
+  
   
   output$table <- DT::renderDataTable({DT::datatable(standardized_Well_WS9(), #MU: When we do the calculations we can put them in one dataset and output that.
                   class = "display", #MU: this is the style of the table
