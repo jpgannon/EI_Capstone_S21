2373a8bcddcfa3f7e01678d8758f1a0825dd136f
marleyg99 <marleygilliam7@gmail.com>
2021-03-08 20:52:50 -0500

    x

--- Litter_and_Respiration/Dashboards/Dash2.0.R
+++ Litter_and_Respiration/Dashboards/Dash2.0.R
@@ -7,20 +7,15 @@ library(tidyr)
 
 
 Litterfall <-
-    read.csv("Z:/Virginia Tech School Work/Current Classes/Capstone/Project directory/EI Capstone/Litter_and_Respiration/Litterfall.csv")
+    read.csv("C:/Users/marle/Desktop/EI Capstone/EI_Capstone_S21/Litter_and_Respiration/Litterfall.csv")
 SoilRespiration <-
-    read.csv("Z:/Virginia Tech School Work/Current Classes/Capstone/Project directory/EI Capstone/Litter_and_Respiration/SoilResp.csv")
+    read.csv("C:/Users/marle/Desktop/EI Capstone/EI_Capstone_S21/Litter_and_Respiration/SoilResp.csv")
 StandLocations <-
-    read.csv("Z:/Virginia Tech School Work/Current Classes/Capstone/Project directory/EI Capstone/Litter_and_Respiration/StandLocations.csv")
+    read.csv("C:/Users/marle/Desktop/EI Capstone/EI_Capstone_S21/Litter_and_Respiration/StandLocations.csv")
 
 CleanSoilResp <- select(SoilRespiration, date, stand, flux, treatment)
 
 CleanSoilResp <- filter(CleanSoilResp, flux < 500, flux >= 0)
-
-#CleanLit <- select()
-
-#FixedLocations <-
-
 
 
 ui <- dashboardPage(
@@ -45,9 +40,16 @@ ui <- dashboardPage(
                 box(width = 4, selectInput("Stand", "Stand:", StandLocations$Site)),
                 fluidRow(box(width = 12, leaflet()%>% addTiles())),
                 h1("Map")),
+        #Timeseries 
         tabItem(tabName = "Litterfall",
+               box(plotoutput("timeseries_plot"), width = 8),
                 box(
-                    #plot code goes here
+                    title = "Timeseries Litterfall"
+                    ,status = "primary"
+                    ,solidHeader = TRUE
+                    ,collapsible = TRUE
+                    ,plotOutput("k", height = "300px")
+        
                 ),
                 h1("Litterfall")),
         tabItem(tabName = "Soil_Respiration",
@@ -64,6 +66,14 @@ server <- function(input, output) {
     output$correlation_plot <- renderPlot({
         plot(CleanSoilResp$treatment, CleanSoilResp[[input$Flux]],
              xlab = "Treatment", ylab = "Flux")
+    })
+    #Litterfall Timeseries server
+    output$k <- renderPlot({
+        ggplot(dataset(),
+               aes(x=Year, y=whole.mass, group=Treatment, colour=Treatment)) +
+         geom_line(aes(size=Treatment)) +
+            geompoint() +
+            labs(title ="Timeseries Litterfall", x = "Year", y = "whole.mass")
     })
     
 }
